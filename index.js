2046108f03299f3d7dfbaf40629193fe0588b9b9
coderminy <122503409+coderminy@users.noreply.github.com>
2025-03-23 22:19:25 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+http.createServer((req, res) => {
+    if (req.url === '/resource') {
+        const filePath = path.join(__dirname, '/resource.js');
+        fs.readFile(filePath, (err, data) => {
+            res.writeHead(200, {'Content-Type': 'text/javascript'});
+            res.end(data);
+        });
+    }
+}).listen(3000);
