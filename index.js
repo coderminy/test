6735a7b96110adc4f9abef547e65cb6a5e8a214d
coderminy <122503409+coderminy@users.noreply.github.com>
2025-03-31 13:45:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ const path = require('path');
 http.createServer((req, res) => {
     if (req.url === '/resource') {
         const filePath = path.join(__dirname, '/resource.js');
-        setTimeout(() => {
           fs.readFile(filePath, (err, data) => {
             res.writeHead(200, {'Content-Type': 'text/javascript'});
             res.end(data);
         });
-        }, 10000)
     }
 }).listen(80);
